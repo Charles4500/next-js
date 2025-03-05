68b8e9b2efa493dfa52f653e016f35a646aee2f2
CharlesKibet <charlesbiegon973@gmail.com>
2025-03-05 16:55:08 +0300

    feature
    Feature to get the night and day icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import { format, fromUnixTime, parseISO } from 'date-fns';
 import Container from '@/components/Container';
 import { convertKelvinToCelsius } from '@/utils/convertToCelcius';
+import WeatherIcon from '@/components/WeatherIcon';
 interface WeatherDetail {
   dt: number;
   main: {
@@ -91,7 +92,7 @@ export default function Home() {
       <Navbar />
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
         <section className="space-y-4">
-          <div>
+          <div className="space-y-2">
             <h2 className="flex gap-1 text-2xl items-end">
               <p>{format(parseISO(firstData?.dt_txt ?? ''), 'EEEE')}</p>
               <p className="text-lg">
@@ -99,6 +100,7 @@ export default function Home() {
               </p>
             </h2>
             <Container className="gap-10 px-6 items-center">
+              {/*temperature*/}
               <div className="flex flex-col px-4">
                 <span className="text-5xl">
                   {convertKelvinToCelsius(firstData?.main.temp ?? 296)}°
@@ -120,6 +122,21 @@ export default function Home() {
                     °↑
                   </span>
                 </p>
+              </div>
+              {/* time and weather icon */}
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((d, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
+                  >
+                    <p className="whitespace-nowrap">
+                      {format(parseISO(d.dt_txt), 'h:mm a')}
+                    </p>
+                    <WeatherIcon iconName={d.weather[0].icon} />
+                    <p>{convertKelvinToCelsius(d?.main.temp ?? 0)}°</p>
+                  </div>
+                ))}
               </div>
             </Container>
           </div>
